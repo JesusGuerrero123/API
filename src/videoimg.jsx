bc6f90127d6d8480b64f448e75dc8f5fa656aada
JesusGuerrero <hp@equipo2.com>
2023-06-07 00:11:49 -0600

    mi api

---
+++ src/videoimg.jsx
@@ -0,0 +1,48 @@
+
+
+const images = [
+    {
+        src: "https://www.freetogame.com/g/521/thumbnail.jpg",
+        Card: '1'
+    },
+    {
+        src: "https://www.freetogame.com/g/517/thumbnail.jpg",
+        Card: '1'
+    },
+    {
+        src: "https://www.freetogame.com/g/516/thumbnail.jpg",
+        Card: '1'
+    },
+    {
+        src: "https://www.freetogame.com/g/508/thumbnail.jpg",
+        Card: '1'
+    },
+    {
+        src: "https://www.freetogame.com/g/345/thumbnail.jpg",
+        Card: '1'
+    },
+    {
+        src: "https://www.freetogame.com/g/427/thumbnail.jpg",
+        Card: '1'
+    },
+    {
+        src: "https://www.freetogame.com/g/525/thumbnail.jpg",
+        Card: '1'
+    },
+    {
+        src: "https://www.freetogame.com/g/475/thumbnail.jpg",
+        Card: '1'
+    },
+    {
+        src: "https://www.freetogame.com/g/523/thumbnail.jpg",
+        Card: '1'
+    },
+    {
+        src: "https://www.freetogame.com/g/340/thumbnail.jpg",
+        Card: '1'
+    },
+   
+    
+];
+
+export {images}
